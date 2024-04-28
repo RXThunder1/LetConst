cfbdb6914defebc5ba5dc22836967b4497572bcb
RXThunder1 <162224845+RXThunder1@users.noreply.github.com>
2024-04-27 21:50:55 -0700

    added letConst.js

---
+++ letConst.js
@@ -0,0 +1,6 @@
+const PI = 3.14
+
+// var allows reassign and redeclare, while let cannot redeclare. Hoisted variables can be called with var, while let creates block scope
+// var allows reassign and redclare, while const prevents usage of both these things. The details between the two are similar to the prior question
+// let allows reassign while const prevents it
+// variables are lifted to the top of the scope they are declared in. Function declarations can also be hoisted.
